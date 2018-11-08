8dadc5028b635da8be49aa731a8a29eadaeab91c
TJ McKinley <t.mckinley@exeter.ac.uk>
2018-11-08 23:24:30 +0000

    Ported slides over from BIOM4025 notes for GLMMs (not formatted correctly yet and needs work, but at least compiles to PDF using beamer).

---
+++ GLMMSlides/compile.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# -a compiles slides and handout
+
+while getopts :a: option
+do
+    case "${option}"
+    in
+        a) compileall=${OPTARG};;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
+
+if [ ! -z ${compileall+x} ]; then
+    if [ $compileall != "0" ] && [ $compileall != "1" ]; then
+        echo "compileall set incorrectly (should be 0 or 1)"
+        exit 1
+    fi
+else
+    compileall="0"
+fi
+
+R -e "rmarkdown::render('GLMMSlides.Rmd')"
+
+if [ $compileall == "1" ]; then
+    pdfannotextractor GLMMSlides.pdf
+    compile="\def\filename{GLMMSlides.pdf} \input{handout.tex}"
+    pdflatex $compile
+    mv handout.pdf GLMMHandout.pdf
+    rm *.pax *.aux *.log
+fi
+
